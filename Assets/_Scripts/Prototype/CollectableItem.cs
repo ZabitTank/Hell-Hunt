fc5b236bc5ebed4030305161d724120c9e430aec
ZabitTank <95601488+ZabitTank@users.noreply.github.com>
2022-12-18 00:16:02 +0700

    refactor project structure

---
+++ Assets/_Scripts/Prototype/CollectableItem.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableItem : MonoBehaviour
+{
+    [SerializeField]
+    private Item item;
+
+    void Start()
+    {
+        
+    }
+
+}
